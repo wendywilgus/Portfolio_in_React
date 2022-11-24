343c291f369d2eb949403b05b57f7cf788225036
Wendy Wilgus <wilguswendy@gmail.com>
2022-11-23 22:36:15 -0500

    final commit

--- src/App.js
+++ src/App.js
@@ -1,40 +1,38 @@
-import logo from './logo.svg';
-import * as React from 'react';
-import './App.css';
-import { Route, BrowserRouter } from 'react-router-dom';
-import Resume from './pages/Resume';
-import Home from './pages/Home';
-import About from './pages/About';
-import Portfolio from './pages/Portfolio';
-import Contact from './pages/Contact';
-import Footer from './components/Footer';
-import '../src/stylesheets/home.css';
-import '../src/stylesheets/header-footer.css';
-import '../src/stylesheets/carousel.css';
-import '../src/stylesheets/portfolio-resume.css'
-import AOS from 'aos';
-import 'aos/dist/aos.css'; 
+import logo from "./logo.svg";
+import * as React from "react";
+import "./App.css";
+import { Route, BrowserRouter } from "react-router-dom";
+import Resume from "./pages/Resume";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Portfolio from "./pages/Portfolio";
+import Contact from "./pages/Contact";
+import Footer from "./components/Footer";
+import "../src/stylesheets/home.css";
+import "../src/stylesheets/header-footer.css";
+import "../src/stylesheets/carousel.css";
+import "../src/stylesheets/portfolio-resume.css";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 AOS.init({
-  duration: 500
+	duration: 500,
 });
 function App() {
-  return (
-    <div className="App">
-        
-         <BrowserRouter>
-         
-           <Route path='/' exact component={Home}/>
-           <Route path='/about' exact component={About}/>
-           <Route path='/portfolio' exact component={Portfolio}/>
-           <Route path='/resume' exact component={Resume}/>
-           <Route path='/contact' exact component={Contact}/>         
-         </BrowserRouter>
+	return (
+		<div className="App">
+			<BrowserRouter>
+				<Route path="/Portfolio_in_React/" exact component={Home} />
+				<Route path="/" exact component={Home} />
+				<Route path="/about" exact component={About} />
+				<Route path="/portfolio" exact component={Portfolio} />
+				<Route path="/resume" exact component={Resume} />
+				<Route path="/contact" exact component={Contact} />
+			</BrowserRouter>
 
-         <Footer/>
-    </div>
-  )
-};
-
+			<Footer />
+		</div>
+	);
+}
 
 export default App;
